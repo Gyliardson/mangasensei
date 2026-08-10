bef959de5a2e8816435a6bb9db1e70e2d1c7d7bb
Gyliardson Keitison <100867910+Gyliardson@users.noreply.github.com>
2026-08-10 09:56:36 -0300

    test(frontend): isolate legacy UI assertions to pt-BR

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,5 +1,9 @@
 import "@testing-library/jest-dom/vitest";
 
+beforeEach(() => {
+  window.localStorage.setItem("mangasensei.ui.locale", "pt-BR");
+});
+
 afterEach(() => {
   document.body.innerHTML = "";
 });
